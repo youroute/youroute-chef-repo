b754dcc571e41fc9b10731446cb342c1ed76b896
Nikita Simakov <everm1nd.mail@gmail.com>
2012-05-23 19:34:45 +0400

    production role fix

--- roles/production.rb
+++ roles/production.rb
@@ -3,4 +3,6 @@ description "Production Machine"
 run_list(
   "recipe[youroute::production]"
 )
-# override_attributes "locale" => { "lang" => [ "en_GB.UTF-8 UTF-8", "ru_RU.UTF-8 UTF-8" ] }
+override_attributes "zsh" => { "users" => "ubuntu" }
+override_attributes "ohmyzsh" => { "users" => "ubuntu" }
+override_attributes "rbenv" => { "group_users" => "ubuntu" }
