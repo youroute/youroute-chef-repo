b66cdaf9bcaac4b96e1d6e76c4f824fb985ced2e
Nikita Simakov <everm1nd.mail@gmail.com>
2012-06-22 15:42:12 +0400

    removed excess override_attributes from roles

--- roles/production.rb
+++ roles/production.rb
@@ -3,6 +3,3 @@ description "Production Machine"
 run_list(
   "recipe[youroute::production]"
 )
-default_attributes({ "zsh" => {"users" => "ubuntu"}})
-default_attributes({ "ohmyzsh" => {"users" => "ubuntu"}})
-default_attributes({ "rbenv" => {"group_users" => "ubuntu"}})
--- roles/staging.rb
+++ roles/staging.rb
@@ -3,4 +3,3 @@ description "Staging and Monitoring Machine"
 run_list(
   "recipe[youroute::staging]"
 )
-override_attributes "rbenv" => { "group_users" => [ 'vagrant', 'gitlabhq' ] }
